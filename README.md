dae6e237c54152197854f4c7ada5e9623af849ca
Anders Alavik <anders@alavik.se>
2022-05-13 10:07:33 +0200

    fix

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # domain-registrar-check
 
+
+## Info
+
 Script to find out Registrar and Nameservers from a file with domainnames.
 
 ## How to
 
+### Install requirements
+
 ```bash
-% python3 check.py
+$ python3 -m pip install -r requirements.txt
+```
+
+### Run
+
+```bash
+$ python3 check.py -f domains-example.txt --json
 ```
--- check.py
+++ check.py
@@ -1,22 +1,128 @@
+from email.policy import default
+import json
+import optparse
+import sys
 import whois
-import time
-
-domains = open("domains.txt",'r')
-
+import optparse
+import logging
 
 
-for domain in domains.readlines():
-	print('')
-	print('Kontrollerar:',domain.strip('\n'))
-	try:
-		w = whois.whois(domain.strip('\n')
-		print('Registrar:',w['registrar'])
-		print('Status:',w['status'])
-		print('Name server:',w['name_servers'])
-	except:
-		print('Hittar inte')
-	
-	time.sleep(0.5)
-	
-	
-	
+
+
+
+class CheckDomain(object):
+    
+    DOMAINS = {}
+    
+    def __init__(self):
+        self.use_qnichost = False
+        
+    def run(self,options):
+        loglevel = options['loglevel']
+        numeric_level = getattr(logging, loglevel.upper(), None)
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=numeric_level)
+        
+        
+        logging.debug('Started')
+        
+        domain_list=[]
+        if options is None:
+            options = {}
+        
+        logging.debug(options)
+        
+        if 'domainfile' in options and options['domainfile'] is not None:
+            domain_list = self.processDomainListFile(options['domainfile'])
+            
+        
+        
+        self.processDomainList(domain_list)
+        
+        
+        if options['json'] is True:
+            self.jsonOut()
+        logging.debug('Finished')
+        
+
+    def domainCheck(self, domain):
+        logging.debug('Kontrollerar: %s', domain)
+        domain_info = dict()
+        
+        try:
+            
+            domainc = whois.whois(domain)
+        except:
+            logging.debug('Hittar inte')
+            domain_info['status'] = 'fail'
+            pass
+        else:
+            logging.debug('Registrar: %s', domainc['registrar'])
+            logging.debug('Status: %s', domainc['status'])
+            logging.debug('Name server %s:', domainc['name_servers'])
+            domain_info['registrar'] = domainc['registrar']
+            domain_info['status'] = domainc['status']
+            
+            servers = list()
+            for server in domainc['name_servers']:
+                servers.append(server)
+            
+            domain_info['name_servers'] = servers
+        
+        
+        
+
+        return domain_info
+    
+    def processDomainListFile(self,file):
+        logging.debug('processar domain list file')
+        domains = open(file, 'r')
+        domains = [line.rstrip('\n') for line in domains]
+        
+        
+        
+        return domains
+    
+    def processDomainList(self,domainlist):
+        logging.debug('processar domain list')
+        for domain in domainlist:
+            self.DOMAINS[domain] = self.domainCheck(domain)
+            
+    def jsonOut(self):
+        logging.debug('Printar JSON')
+        print(json.dumps(self.DOMAINS,indent = 4))
+        
+            
+        
+        
+
+
+def parse_command_line(argv):
+    
+    usage = "usage: %prog [options] name"
+    parser = optparse.OptionParser(add_help_option=False, usage=usage)
+
+    parser.add_option("-f", "--file", action="store",
+                      dest="domainfile",type="string",
+                      help="Check using file.")
+    parser.add_option("-j", "--json", action="store_true",
+                      dest="json",default=False,
+                      help="Output JSON")
+    parser.add_option("--log", action="store",
+                      dest="loglevel",type="string",
+                      help="--log debug för att skriva ut i console", default="info")
+
+    parser.add_option("-h", "--help", action="help")
+    return parser.parse_args(argv)
+
+
+
+    
+
+if __name__ == "__main__":
+
+    flags = 0
+    domainCheck = CheckDomain()
+    options, args = parse_command_line(sys.argv)
+    
+    domainCheck.run(options.__dict__)
+
